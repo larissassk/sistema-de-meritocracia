1bd7f09555b6e4a0b87ef43f27aa6d526c2c8262
larissassk <larissasantos200702@gmail.com>
2024-03-25 15:13:43 -0300

    Criei o style que faz a parte de estilizar a página

--- sistema.php
+++ sistema.php
@@ -5,21 +5,91 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Sistema de Salário</title>
 </head>
+<style>
+body {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    height: 100vh;
+    margin: 0;
+    background-color: #465C8B;
+}
+
+.formulario {
+    background-color: #DFDCE3;
+    max-width: 600px;
+    margin: 20px auto;
+    padding: 30px;
+    border: 2px solid #93C178;
+    border-radius: 5px;
+}
+
+.formulario h1 {
+    color: #DFDCE3;
+    text-align: center;
+    margin-bottom: 20px;
+}
+
+.formulario h2,
+.formulario h3 {
+    color: #555;
+    margin-bottom: 10px;
+}
+
+.formulario label {
+    display: block;
+    margin-bottom: 5px;
+    color: #333;
+}
+
+.formulario input {
+    width: calc(100% - 16px);
+    padding: 8px;
+    margin-bottom: 10px;
+    box-sizing: border-box;
+    border: 1px solid #ccc;
+    border-radius: 3px;
+    font-size: 14px;
+}
+
+.formulario input[type="submit"] {
+    background-color: #93C178;
+    color: white;
+    border: none;
+    cursor: pointer;
+}
+
+.formulario input[type="submit"]:hover {
+    background-color: #465C8B;
+}
+
+.mensagem {
+    background-color: #DFDCE3;
+    max-width: 400px;
+    margin: 20px auto;
+    padding: 20px;
+    border: 1px solid #93C178;
+    border-radius: 5px;
+    color: #333;
+    text-align: center;
+}
+</style>
 <body>
-    <form action="" method="post">
-        <h1>Calculadora de Salário de Vendedor</h1>
+    <form class="formulario" method="post">
+        <h1>Sistema de Pagamento Baseado em Metas</h1>
         <label for="nome">Nome do Vendedor:</label>
         <input type="text" id="nome" name="nome" required><br><br>
 
-        <label for="vendas_semanais">Vendas Semanais Alcançadas:</label>
+        <label for="vendas_semanais">Meta semanal:</label>
         <input type="number" id="vendas_semanais" name="vendas_semanais" min="0" step="0.01" required><br><br>
 
-        <label for="vendas_mensais">Vendas Mensais Alcançadas:</label>
+        <label for="vendas_mensais">meta mensal:</label>
         <input type="number" id="vendas_mensais" name="vendas_mensais" min="0" step="0.01" required><br><br>
 
         <button type="submit">Calcular</button>
     </form>
 
+    <div class="mensagem" >
     <?php
     if ($_SERVER["REQUEST_METHOD"] == "POST") {
         // Pega os dados do formulário
@@ -74,5 +144,6 @@
         }
     }
     ?>
+    </div>
 </body>
 </html>
